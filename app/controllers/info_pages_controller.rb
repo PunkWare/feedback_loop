95ae261b1557b51b4c78ac1c0df63c82ccf95d3e
PunkWare <punkware@free.fr>
2012-04-28 21:55:37 +0200

    Use icons on home view for private & anonymous surveys.

--- app/controllers/info_pages_controller.rb
+++ app/controllers/info_pages_controller.rb
@@ -1,6 +1,6 @@
 class InfoPagesController < ApplicationController
 	def home
-		@surveys = Survey.where(:available => true, :private => false).order("created_at DESC").paginate(page: params[:page]) if signed_in?
+		@surveys = Survey.where(available: true).order("created_at DESC").paginate(page: params[:page]) if signed_in?
 	end
 
 	def help
